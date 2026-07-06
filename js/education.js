a3bd5c08894f3ce8b4cd98b53c8a0250089a2e44
Sayan Mondal <analystsayan1@gmail.com>
2026-07-06 18:56:09 +0530

    education result updated

--- js/education.js
+++ js/education.js
@@ -5,6 +5,7 @@ const educationData = [
         schoolName: "Maulana Abul Kalam Azad University of Technology",
         course: "Information Technology",
         stream: "Data Science",
+        result: "9.15",
         year: "2023"
     },
     {
@@ -13,6 +14,7 @@ const educationData = [
         schoolName: "Lego RBRK Vidyapith (HighSchool)",
         course: "Higher Secondary Education",
         stream: "Science",
+        result: "80.6%",
         year: "2020"
     },
     {
@@ -21,6 +23,7 @@ const educationData = [
         schoolName: "Aswinkota Vidyasagar Vidyamandir",
         course: "Secondary Education",
         stream: "General Science",
+        result: "83.7%",
         year: "2018"
     }
 ];
@@ -28,7 +31,7 @@ const educationData = [
 function renderEducation() {
     const container = document.getElementById("educationContainer");
 
-    educationData.forEach(({ imgSrc, alt, schoolName, course, stream, year }) => {
+    educationData.forEach(({ imgSrc, alt, schoolName, course, stream, result, year }) => {
         const card = document.createElement("div");
         card.classList.add("school-card");
 
@@ -40,6 +43,7 @@ function renderEducation() {
             <h3 class="school-name">${schoolName}</h3>
             <p class="course">Course: ${course}</p>
             <p class="stream">Stream: ${stream}</p>
+            <p class="stream">CGPA/Percentage: ${result}</p>
             <p class="year">Year: ${year}</p>
         </div>
         `;
